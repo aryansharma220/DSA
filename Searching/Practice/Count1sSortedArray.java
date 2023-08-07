462dfe9a2eea39e0c6408175e1c5d8310d872d11
Aryan <aryansharma220318@gmail.com>
2023-08-07 18:54:20 +0530

    Added Articles of Binary Sort

--- Searching/Practice/Count1sSortedArray.java
+++ Searching/Practice/Count1sSortedArray.java
@@ -1,6 +1,6 @@
 public class Count1sSortedArray {
     public static void main(String[] args) {
-        int arr[]={0,0,0,0,1,1,1,1,1,1};
+        int arr[]={0,0,0,0,0};
         int result=naive(arr);
         System.out.println(result);
     }
